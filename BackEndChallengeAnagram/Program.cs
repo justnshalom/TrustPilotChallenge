ec08ad830b36a66af7fa543166a5371f152b450a
justnshalom <justnshalom@gmail.com>
2018-06-10 23:18:12 +0530

    up

--- BackEndChallengeAnagram/Program.cs
+++ BackEndChallengeAnagram/Program.cs
@@ -1,127 +1,129 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BackEndChallengeAnagram
 {
     class Program
     {
-        private static List<string> allowedWordList;
+        private static Dictionary<string, Dictionary<char,int >> allowedWordList;
         private static int wordsLength;
         private static int anagramLength;
-        private static Dictionary<string,int> chracterGroup;
+        private static int characterLength;
+        private static int maxLevel=3;
+        private static double phraseValue;
+        private static Dictionary<char,int> chracterGroup;
+        private static List<double> integerwordsList;
+        private static List<string> stringwordsList;
+        private static Stopwatch timer = Stopwatch.StartNew();
+        private static IFormatProvider culture = CultureInfo.CreateSpecificCulture("en");
         static void Main(string[] args)
         {
             var anagramPhrase = "poultry outwits ants";
             ////anagramPhrase = "printout stout yawls";
             var encryptedString = Utilities.MD5Hash(anagramPhrase);
-            var anagramPhraseCharacters = anagramPhrase.ToCharArray().Select(c => c.ToString()).ToList();
+            var anagramPhraseCharacters = anagramPhrase.ToCharArray().ToList();
             anagramLength = anagramPhraseCharacters.Count();
+            var alphabetsCharacters = anagramPhraseCharacters.Where(x => x != ' ');
+            characterLength = alphabetsCharacters.Count();
             wordsLength = anagramPhrase.Split(' ').Length;
-            chracterGroup = anagramPhraseCharacters.GroupBy(x => x).ToList().ToDictionary(x => x.FirstOrDefault(), x => x.Count());
-            var allAlphabetsInAnagram = anagramPhraseCharacters.Where(x => x != " ").Distinct().ToList();
+            chracterGroup = alphabetsCharacters.GroupBy(x => x).ToList().ToDictionary(x => x.FirstOrDefault(), x => x.Count());
+            var allAlphabetsInAnagram = anagramPhraseCharacters.Where(x => x != ' ').Distinct().ToList();
             ////var mutationstring = GetPermutations(anagramPhrase);
             var wordList= File.ReadLines("../../wordlist.text").ToArray();
             var expectedStringLength = anagramLength - ((wordsLength - 1) * 2);
-            allowedWordList = wordList.Where(x => x.Length <= expectedStringLength && x.ToCharArray().Select(c => c.ToString()).ToList().All(y => allAlphabetsInAnagram.Contains(y)) && !x.ToCharArray().Select(c => c.ToString()).GroupBy(g => g).ToList().ToDictionary(d => d.FirstOrDefault(), d => d.Count()).Any(z => z.Value > chracterGroup[z.Key])).Distinct().OrderByDescending(l => l.Length).ToList();
+            
+            var alphabetsList = allAlphabetsInAnagram.Select((x, i) => new { Item = x, Index = i }).ToDictionary(x => x.Item, x => (Math.Pow(10,x.Index)));
 
-            RecursivePhraseFinder(string.Empty, 0);
+            allowedWordList = wordList.Distinct().OrderByDescending(l => l.Length).ToList().ToDictionary(x => x, x => x.ToCharArray().Select(c => c).GroupBy(g => g).ToList().ToDictionary(d => d.FirstOrDefault(), d => d.Count())).Where(x => x.Key.Length <= expectedStringLength && !x.Value.Any(y => !allAlphabetsInAnagram.Contains(y.Key)) && !x.Value.Any(z => z.Value > chracterGroup[z.Key])).ToDictionary(x=>x.Key,x=>x.Value);
+            stringwordsList= allowedWordList.Select(x => x.Key).ToList();
+            integerwordsList = allowedWordList.Select(x => x.Value.Sum(y => alphabetsList[y.Key] * y.Value)).ToList();
 
-            Console.Write(encryptedString);
+            phraseValue = chracterGroup.Sum(x => alphabetsList[x.Key] * x.Value);
 
+            timer = Stopwatch.StartNew();
+            ////RecursivePhraseFinder(string.Empty, 0);
+            Console.WriteLine("started finding first one");
+            RecursivePhraseFinder(0, new List<int>(),0);
             Console.WriteLine("\nPress any key to exit.");
             Console.ReadLine();
+
         }
-        
+
         private static List<string> secretphases = new List<string> { "e4820b45d2277f3844eac66c903e84be", "23170acc097c24edb98fc5488ab033fe", "665e5bcb0c20062fe8abaaf4628bb154" };
-       
+        public static bool FoundAllWords = false;
         public static List<string> List=new List<string>();
-        private static void RecursivePhraseFinder(string phrase, int level,List<string> usedWords=null)
+        public static List<List<int>> permutations = new List<List<int>>();
+        private static void RecursivePhraseFinder(int level, List<int> usedwords = null, double currentphraseValue = 0)
         {
-            if (level == wordsLength)
+            if (!FoundAllWords)
             {
-                var encryptedString = Utilities.MD5Hash(phrase);
-                if (secretphases.Contains(encryptedString))
+                if (currentphraseValue < phraseValue)
                 {
-                    List.Add(phrase);
-                }
-            }
-            else
-            {
-                var newLevel = level + 1;
-                var expectedStringLength = 20 - phrase.Length;
-                var wordsAllowedForNow= allowedWordList.Where(x => x.Length <= expectedStringLength).ToList();
+                    var newLevel = level + 1;
+                    if (newLevel <= maxLevel)
+                    {
+                        var degreeOfParallelism = Environment.ProcessorCount / 2;
 
-                Parallel.ForEach(
-                    wordsAllowedForNow,
-                    word =>
+                        var tasks = new Task[degreeOfParallelism];
+
+                        for (int taskNumber = 0; taskNumber < degreeOfParallelism; taskNumber++)
                         {
-                            ////"printout stout yawls";
-                            if (phrase == "printout stout" && word == "yawls")
-                            {
+                            int taskNumberCopy = taskNumber;
 
-                            }
-                            if (usedWords == null || !usedWords.Contains(word))
-                            {
-                                var usedWordsNow = new List<string>();
-                                if (usedWords != null)
+                            tasks[taskNumber] = Task.Factory.StartNew(
+                                () =>
                                 {
-                                    usedWordsNow.AddRange(usedWords);
-                                }
-
-                                usedWordsNow.Add(word);
-                                var newPhrase = level == 0 ? word : phrase + " " + word;
-                                if (newLevel == 3 && newPhrase.Length == 20)
-                                {
-                                    Console.WriteLine("Checking -> " + newPhrase);
-                                }
-                                if (newPhrase.StartsWith("printout") && newPhrase.Length == 20)
-                                {
-
-                                }
-                                if (newPhrase == "printout stout")
-                                {
-
-                                }
-                                if (!newPhrase.StartsWith("printout"))
-                                {
-
-                                }
-                                if (phrase == "printout stout" && word == "yawls")
-                                {
-
-                                }
-                                ////Avoided not match length characters
-                                if ((newLevel != wordsLength && newPhrase.Length <= anagramLength) || (newLevel == wordsLength && newPhrase.Length == anagramLength))
-                                {
-                                    if (phrase == "printout" && word == "stout")
+                                    var max = integerwordsList.Count * (taskNumberCopy + 1) / degreeOfParallelism;
+                                    for (int i = integerwordsList.Count * taskNumberCopy / degreeOfParallelism;
+                                        i < max;
+                                        i++)
                                     {
-
-                                    }
-                                    var newPhraseCharacters = newPhrase.ToCharArray().Select(c => c.ToString()).ToList();
-                                    var newPhraseChracterGroup = newPhraseCharacters.GroupBy(x => x).ToList().ToDictionary(x => x.FirstOrDefault(), x => x.Count());
-                                    if (!newPhraseChracterGroup.Any(x => x.Value > chracterGroup[x.Key]))
-                                    {
-                                        if (phrase == "printout stout" && word == "yawls")
+                                        var currentvalue = integerwordsList[i] + currentphraseValue;
+                                        if (currentvalue <= phraseValue)
                                         {
+                                            var usedWordsNow = new List<int>();
+                                            usedWordsNow.AddRange(usedwords);
+                                            usedWordsNow.Add(i);
+
+                                            RecursivePhraseFinder(newLevel, usedWordsNow, currentvalue);
                                         }
 
-                                        Task.Run(() => RecursivePhraseFinder(newPhrase, newLevel, usedWordsNow));
                                     }
-                                }
-                            }
-                            else
-                            {
-                                if (phrase == "printout stout" && word == "yawls")
-                                {
+                                });
+                        }
 
-                                }
-                            }
-                        });
+                        Task.WaitAll(tasks);
+                    }
+                }
+                else if (usedwords.Any() && currentphraseValue == phraseValue)
+                {
+                    var phrase = string.Join(" ", usedwords.Select(x => stringwordsList[x]));
+                    var encryptedString = Utilities.MD5Hash(phrase);
+                    if (secretphases.Contains(encryptedString))
+                    {
+                        Console.Write("\nFound " + encryptedString + " of " + phrase + " in " + timer.Elapsed.TotalSeconds.ToString("F4", culture) + " seconds");
+                        List.Add(phrase);
+                        if (List.Count() < secretphases.Count())
+                        {
+                            Console.WriteLine("started finding next one");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Founded All phases");
+                            Console.WriteLine("\nPress any key to exit.");
+                            Console.ReadLine();
+                        }
+                    }
+                }
             }
+            
         }
     }
 }
